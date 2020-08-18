44b042fbd81634c08b413b9aff69b6f994d4e11c
Craig Kaley <craig@caucusroom.com>
2020-08-17 20:48:47 -0600

    Trying to solve Heroku Deploy Issues

---
+++ build/precache-manifest.d4f0f4e1f628fabe7d7a27115fa51d66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8c908781cd9496c36f692bd3aa5077",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1280684f6abbde9d6e95",
+    "url": "/static/css/main.e93be599.chunk.css"
+  },
+  {
+    "revision": "18a65ae9812a9ee130b3",
+    "url": "/static/js/2.84bdf0c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84bdf0c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1280684f6abbde9d6e95",
+    "url": "/static/js/main.aec2c160.chunk.js"
+  },
+  {
+    "revision": "896b3cdc65503677e55e",
+    "url": "/static/js/runtime-main.830e4b6e.js"
+  }
+]);
